ddc7019085af9112dd5f02be6e8f30f9db5697a0
Pythonidae Julius Domitor <misha.smit@xs4all.nl>
2018-11-06 00:10:03 +0100

    Changed to fit FileManager

--- src/com/alchemi/as/AuctionStorm.java
+++ src/com/alchemi/as/AuctionStorm.java
@@ -1,44 +1,34 @@
 package com.alchemi.as;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.alchemi.al.Library;
 import com.alchemi.as.cmds.CommandBid;
 import com.alchemi.as.cmds.Commando;
+import com.alchemi.as.cmds.FileManager;
 
 import net.milkbowl.vault.economy.Economy;
 
 public class AuctionStorm extends JavaPlugin implements Listener {
 	public String pluginname;
 	public static Economy econ;
-//
+
 	private FileManager fileManager;
 	public FileManager getFileManager() {
 		return fileManager;
 	}
 	private final int MESSAGES_FILE_VERSION = 0;
+	private final int CONFIG_FILE_VERSION = 0;
 	
 	public static AuctionStorm instance;
-	
-	private Map<String, Object> data_defaults = new HashMap<String, Object>();
-	public ArrayList<Player> banned_players = new ArrayList<Player>(); 
 
 	@Override
 	public void onEnable() {
 		instance = this;
-		
-		data_defaults.put("baddies", new ArrayList<Player>());
 		
 		pluginname = getDescription().getName();
 
@@ -54,12 +44,10 @@ public class AuctionStorm extends JavaPlugin implements Listener {
 		//config = getConfig();
 		//checkFileExists(configfile);
 		
-		data = Library.loadExtraConfig(datafile);
-		data.addDefaults(data_defaults);
 		
 		//registry
 		registerCommands();
-		getServer().getPluginManager().registerEvents(this, this);
+		//getServer().getPluginManager().registerEvents(this, this);
 
 //van martijnpu:
 		saveDefaultConfig();
@@ -67,28 +55,19 @@ public class AuctionStorm extends JavaPlugin implements Listener {
 		getFileManager().saveDefaultMessagesConfig();
 
 		if(!fileManager.getMessagesConfig().isSet("File-Version-Do-Not-Edit") || !fileManager.getMessagesConfig().get("File-Version-Do-Not-Edit").equals(MESSAGES_FILE_VERSION)) {
-			Messages.sendConsole("Your messages file is outdated! Updating...");
+			Library.print("Your messages file is outdated! Updating...", pluginname);
 			fileManager.updateConfig("messages.yml");
 			fileManager.getMessagesConfig().set("File-Version-Do-Not-Edit", MESSAGES_FILE_VERSION);
 			fileManager.saveMessagesConfig();
-			Messages.sendConsole("File successfully updated!");
+			Library.print("File successfully updated!", pluginname);
 		}
 		if(!getConfig().isSet("File-Version-Do-Not-Edit") || !getConfig().get("File-Version-Do-Not-Edit").equals(CONFIG_FILE_VERSION)) {
-			Messages.sendConsole("Your config file is outdated! Updating...");
+			Library.print("Your messages file is outdated! Updating...", pluginname);
 			fileManager.updateConfig("config.yml");
 			getConfig().set("File-Version-Do-Not-Edit", CONFIG_FILE_VERSION);
 			saveConfig();
-			Messages.sendConsole("File successfully updated!");
+			Library.print("File successfully updated!", pluginname);
 		}
-		
-	}
-	
-	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
-		Player player = e.getPlayer();
-		String playername = player.getName();
-		
-		if (data.getStringList("baddies").contains(playername)) banned_players.add(player);
 		
 	}
 	
@@ -98,7 +77,6 @@ public class AuctionStorm extends JavaPlugin implements Listener {
 		if (Queue.current_auction != null) {
 			Queue.current_auction.forceEndAuction("plugin reload", null);
 		}
-		Library.saveExtraConfig(datafile, data);
 		saveConfig();
 		Library.print("I don't wanna go...", pluginname);
 		
--- src/com/alchemi/as/cmds/FileManager.java
+++ src/com/alchemi/as/cmds/FileManager.java
@@ -1,23 +1,29 @@
-package NL.martijnpu.MariopartyPlugin;
+package com.alchemi.as.cmds;
 
-import NL.martijnpu.MariopartyPlugin.enums.Category;
-import NL.martijnpu.MariopartyPlugin.gameHandler.WorldData;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Level;
+import com.alchemi.as.AuctionStorm;
 
 public class FileManager {
 
-    private final main plugin;
+    private final AuctionStorm plugin;
 
     private FileConfiguration messagesConfig = null;
     private File messagesConfigFile = null;
 
-    public FileManager(main plugin) {
+    public FileManager(AuctionStorm plugin) {
         this.plugin = plugin;
     }
 
@@ -29,9 +35,9 @@ public class FileManager {
      * messages.yml
      */
 
-    public main getMain(){ return plugin;}
+    public AuctionStorm getMain(){ return plugin;}
 
-    void saveDefaultMessagesConfig() {
+    public void saveDefaultMessagesConfig() {
         if(messagesConfigFile == null) {
             messagesConfigFile = new File(plugin.getDataFolder(), "messages.yml");
         }
@@ -40,7 +46,7 @@ public class FileManager {
         }
     }
 
-    FileConfiguration getMessagesConfig() {
+    public FileConfiguration getMessagesConfig() {
         if(messagesConfig == null) {
             reloadMessagesConfig();
         }
@@ -55,7 +61,8 @@ public class FileManager {
 
         // Look for defaults in the jar
         try {
-            Reader defConfigStream = new InputStreamReader(plugin.getResource("messages.yml"));
+            
+        	Reader defConfigStream = new InputStreamReader(plugin.getResource("messages.yml"));
             if(defConfigStream != null) {
                 YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
                 messagesConfig.setDefaults(defConfig);
@@ -66,7 +73,7 @@ public class FileManager {
         }
     }
 
-    void saveMessagesConfig() {
+    public void saveMessagesConfig() {
         if(messagesConfig == null || messagesConfigFile == null) {
             return;
         }
@@ -75,5 +82,45 @@ public class FileManager {
         } catch(IOException ex) {
             plugin.getLogger().log(Level.SEVERE, "Could not save config to " + messagesConfigFile, ex);
         }
+    }
+    
+    public void updateConfig(String file) {
+        HashMap<String, Object> newConfig = getConfigVals(file);
+        FileConfiguration c;
+        if(file.equals("config.yml")) {
+            c = plugin.getConfig();
+        } else {
+            c = getMessagesConfig();
+        }
+        for(String var : c.getKeys(false)) {
+            newConfig.remove(var);
+        }
+        if(newConfig.size() != 0) {
+            for(String key : newConfig.keySet()) {
+                c.set(key, newConfig.get(key));
+            }
+            try {
+                c.save(new File(plugin.getDataFolder(), file));
+            } catch(IOException ignored) {
+            }
+        }
+    }
+    
+    private HashMap<String, Object> getConfigVals(String file) {
+        HashMap<String, Object> var = new HashMap<>();
+        YamlConfiguration config = new YamlConfiguration();
+        try {
+            config.loadFromString(stringFromInputStream(AuctionStorm.class.getResourceAsStream("/" + file)));
+        } catch(InvalidConfigurationException ignored) {
+        }
+        for(String key : config.getKeys(false)) {
+            var.put(key, config.get(key));
+        }
+        return var;
+    }
+    
+    @SuppressWarnings("resource")
+    private String stringFromInputStream(InputStream in) {
+        return new Scanner(in).useDelimiter("\\A").next();
     }
 }
